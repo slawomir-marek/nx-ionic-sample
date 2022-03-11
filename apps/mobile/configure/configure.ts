d84c70a77722817c672b38ec9ffd7f8492c25078
slawomir-marek <slawomir.marek@gmail.com>
2022-03-11 11:07:56 +0100

    Updated native project configuration

--- apps/mobile/configure/configure.ts
+++ apps/mobile/configure/configure.ts
@@ -17,6 +17,21 @@ const updateAndroidProject = async() => {
   console.log(project);
 
   /* Place for our actions */
+  // await project.android?.setPackageName('nx.ionic.sample');
+  // await project.android?.setVersionName('1.0.1');
+
+  const appBuildGradleFile = project.android?.getGradleFile('app/build.gradle');
+
+  const versionCode = await appBuildGradleFile?.getVersionCode();
+  const versionName = await appBuildGradleFile?.getVersionName();
+
+  console.log('version code: ' + versionCode);
+  console.log('version name: ' + versionName);
+
+  await appBuildGradleFile?.setVersionCode(2);
+  await appBuildGradleFile?.setVersionName('2.0');
+
+  project.commit();
 }
 
 updateAndroidProject();
